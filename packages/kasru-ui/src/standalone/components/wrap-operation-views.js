b1e709bfc8bfad7dcdd65f03d32145e411b9f290
PhucPNT <pn.truongphuc@gmail.com>
2018-06-30 17:15:58 +0700

    feat(admin): add tabs to operation view

--- packages/kasru-ui/src/standalone/components/wrap-operation-views.js
+++ packages/kasru-ui/src/standalone/components/wrap-operation-views.js
@@ -5,7 +5,8 @@ import {
   Icon,
   Label,
   Dropdown,
-  Form
+  Form,
+  Menu
 } from "semantic-ui-react";
 import { List } from "immutable";
 
@@ -21,128 +22,163 @@ const SWAGGER2_OPERATION_METHODS = [
 
 const OAS3_OPERATION_METHODS = SWAGGER2_OPERATION_METHODS.concat(["trace"]);
 
+class ByTicketsOperationView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filters: []
+    };
+  }
+
+  handleFilter = (evt, { name, value }) => {
+    console.info(name, value);
+    this.setState({ [name]: value });
+  };
+
+  getPanels(opsByTickets) {
+    const { specSelectors } = this.props;
+    const { filters } = this.state;
+    console.info(filters);
+    const OperationContainer = this.props.getComponent(
+      "OperationContainer",
+      true
+    );
+    return opsByTickets
+      .filter((v, url) => filters.length === 0 || filters.indexOf(url) > -1)
+      .map((ops, url) => {
+        const tag = url.split("/").slice(-1)[0];
+        return {
+          title: {
+            content: (
+              <Header size="medium" as="span">
+                {tag} <Label>{ops.size}</Label>{" "}
+                <a
+                  href={url}
+                  target="_blank"
+                  onClick={e => {
+                    e.stopPropagation();
+                  }}
+                >
+                  <Icon name="external" />
+                </a>
+              </Header>
+            ),
+            key: `title-${tag}`
+          },
+          content: {
+            content: (
+              <div>
+                {ops
+                  .map(op => {
+                    const path = op.get("path");
+                    const method = op.get("method");
+                    const specPath = List(["paths", path, method]);
+
+                    // FIXME: (someday) this logic should probably be in a selector,
+                    // but doing so would require further opening up
+                    // selectors to the plugin system, to allow for dynamic
+                    // overriding of low-level selectors that other selectors
+                    // rely on. --KS, 12/17
+                    const validMethods = specSelectors.isOAS3()
+                      ? OAS3_OPERATION_METHODS
+                      : SWAGGER2_OPERATION_METHODS;
+
+                    if (validMethods.indexOf(method) === -1) {
+                      return null;
+                    }
+
+                    return (
+                      <OperationContainer
+                        key={`${path}-${method}`}
+                        specPath={specPath}
+                        op={op}
+                        path={path}
+                        method={method}
+                        tag={tag}
+                      />
+                    );
+                  })
+                  .toArray()}
+              </div>
+            ),
+            key: `content-${tag}`
+          }
+        };
+      })
+      .toArray();
+  }
+
+  render() {
+    const opsByTickets = this.props.specSelectors.opsByTickets();
+    const tickets = this.props.specSelectors.tickets();
+    const panels = this.getPanels(opsByTickets);
+    return (
+      <div className="kasru-operations-container">
+        <Form.Field>
+          <label>Filter: </label>
+          <Dropdown
+            placeholder="Filter by tickets..."
+            multiple
+            search
+            selection
+            options={tickets
+              .map(url => ({
+                text: url.split("/").slice(-1)[0],
+                key: url,
+                value: url
+              }))
+              .toArray()}
+            name="filters"
+            onChange={this.handleFilter}
+          />
+        </Form.Field>
+        <p>&nbsp;</p>
+        <Accordion
+          fluid
+          panels={panels}
+          exclusive={false}
+          defaultActiveIndex={panels.map((v, k) => k)}
+        />
+      </div>
+    );
+  }
+}
+
 export default function wrapOperations(Operations, system) {
-  class CustomOpertionsView extends Component {
+  class MultipleOperationsView extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        filters: []
+        opView: "tickets"
       };
     }
 
-    handleFilter = (evt, { name, value }) => {
-      console.info(name, value);
-      this.setState({ [name]: value });
-    };
-
-    getPanels(opsByTickets) {
-      const { specSelectors } = this.props;
-      const { filters } = this.state;
-      console.info(filters);
-      const OperationContainer = this.props.getComponent(
-        "OperationContainer",
-        true
-      );
-      return opsByTickets
-        .filter((v, url) => filters.length === 0 || filters.indexOf(url) > -1)
-        .map((ops, url) => {
-          const tag = url.split("/").slice(-1)[0];
-          return {
-            title: {
-              content: (
-                <Header size="medium" as="span">
-                  {tag} <Label>{ops.size}</Label>{" "}
-                  <a
-                    href={url}
-                    target="_blank"
-                    onClick={e => {
-                      e.stopPropagation();
-                    }}
-                  >
-                    <Icon name="external" />
-                  </a>
-                </Header>
-              ),
-              key: `title-${tag}`
-            },
-            content: {
-              content: (
-                <div>
-                  {ops
-                    .map(op => {
-                      const path = op.get("path");
-                      const method = op.get("method");
-                      const specPath = List(["paths", path, method]);
-
-                      // FIXME: (someday) this logic should probably be in a selector,
-                      // but doing so would require further opening up
-                      // selectors to the plugin system, to allow for dynamic
-                      // overriding of low-level selectors that other selectors
-                      // rely on. --KS, 12/17
-                      const validMethods = specSelectors.isOAS3()
-                        ? OAS3_OPERATION_METHODS
-                        : SWAGGER2_OPERATION_METHODS;
-
-                      if (validMethods.indexOf(method) === -1) {
-                        return null;
-                      }
-
-                      return (
-                        <OperationContainer
-                          key={`${path}-${method}`}
-                          specPath={specPath}
-                          op={op}
-                          path={path}
-                          method={method}
-                          tag={tag}
-                        />
-                      );
-                    })
-                    .toArray()}
-                </div>
-              ),
-              key: `content-${tag}`
-            }
-          };
-        })
-        .toArray();
-    }
+    handleMenuClick = (e, { name }) => this.setState({ opView: name });
 
     render() {
-      const opsByTickets = this.props.specSelectors.opsByTickets();
-      const tickets = this.props.specSelectors.tickets();
-      const panels = this.getPanels(opsByTickets);
+      const activeItem = this.state.opView;
+
       return (
-        <div className="kasru-operations-container">
-          <Form.Field>
-            <label>Filter: </label>
-            <Dropdown
-              placeholder="Filter by tickets..."
-              multiple
-              search
-              selection
-              options={tickets
-                .map(url => ({
-                  text: url.split("/").slice(-1)[0],
-                  key: url,
-                  value: url
-                }))
-                .toArray()}
-              name="filters"
-              onChange={this.handleFilter}
+        <div>
+          <Menu color="green" pointing size="large" widths={2}>
+            <Menu.Item
+              name="tickets"
+              active={activeItem === "tickets"}
+              onClick={this.handleMenuClick}
             />
-          </Form.Field>
-          <p>&nbsp;</p>
-          <Accordion
-            fluid
-            panels={panels}
-            exclusive={false}
-            defaultActiveIndex={panels.map((v, k) => k)}
-          />
+            <Menu.Item
+              name="tags"
+              active={activeItem === "tags"}
+              onClick={this.handleMenuClick}
+            />
+          </Menu>
+          {activeItem === "tickets" && (
+            <ByTicketsOperationView {...this.props} />
+          )}
+          {activeItem === "tags" && <Operations {...this.props} />}
         </div>
       );
     }
   }
-
-  return CustomOpertionsView;
+  return MultipleOperationsView;
 }
