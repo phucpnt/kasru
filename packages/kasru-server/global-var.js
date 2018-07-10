4d560be9ec6ff2c402d8d332ed5e929e76e1f969
Phuc PNT <pn.truongphuc@gmail.com>
2018-07-10 18:33:23 +0700

    chore

--- packages/kasru-server/global-var.js
+++ packages/kasru-server/global-var.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports.PORT = process.env.NODE_PORT || 3003;
 module.exports.DIR_SPEC = process.env.DIR_SPEC ? path.resolve(process.env.DIR_SPEC) : process.cwd();
-module.exports.GH_CLIENT_ID = process.env.GH_CLIENT_ID || '';
-module.exports.GH_CLIENT_SECRET = process.env.GH_CLIENT_SECRET || '';
+module.exports.GH_CLIENT_ID = process.env.GH_CLIENT_ID || 'client_id';
+module.exports.GH_CLIENT_SECRET = process.env.GH_CLIENT_SECRET || 'client_secret';
 module.exports.GH_CALLBACK_HOST = process.env.GH_CALLBACK_HOST || 'http://localhost:3003';
 
